eba133c5f98273123f9d1bdeac45db5f286cdfbf
Raoul Rapeli <raoul.rapeli@gmail.com>
2024-07-31 01:07:51 +0300

    Simple end-to-end tests (One test)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     "commonjs": true,
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -23,7 +24,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
